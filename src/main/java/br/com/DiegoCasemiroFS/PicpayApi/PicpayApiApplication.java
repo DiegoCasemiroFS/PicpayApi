c22752d63d068b55db3d432d98cc6256685696a8
DiegoCasemiroFS <dcfs1998@gmail.com>
2024-07-03 15:12:05 -0300

    first commit

---
+++ src/main/java/br/com/DiegoCasemiroFS/PicpayApi/PicpayApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.DiegoCasemiroFS.PicpayApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/DiegoCasemiroFS/PicpayApi/PicpayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.DiegoCasemiroFS.PicpayApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
