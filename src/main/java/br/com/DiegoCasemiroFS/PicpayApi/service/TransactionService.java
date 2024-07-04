fa4cfe742f99379e34512deff59a35e5540da2b1
DiegoCasemiroFS <dcfs1998@gmail.com>
2024-07-04 16:30:46 -0300

    feat: add anotation

--- src/main/java/br/com/DiegoCasemiroFS/PicpayApi/service/TransactionService.java
+++ src/main/java/br/com/DiegoCasemiroFS/PicpayApi/service/TransactionService.java
@@ -7,12 +7,14 @@ import br.com.DiegoCasemiroFS.PicpayApi.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Map;
 
+@Service
 public class TransactionService {
 
     @Autowired
