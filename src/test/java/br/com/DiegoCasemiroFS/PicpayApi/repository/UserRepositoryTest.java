61bd1f5715214593e5eb2fcd550c44d044cbaf86
DiegoCasemiroFS <dcfs1998@gmail.com>
2024-07-17 19:10:23 -0300

    feat: create UserRepositoryTest.java

---
+++ src/test/java/br/com/DiegoCasemiroFS/PicpayApi/repository/UserRepositoryTest.java
@@ -0,0 +1,17 @@
+package br.com.DiegoCasemiroFS.PicpayApi.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Test
+    void findUserByDocument() {
+
+
+
+    }
+}
