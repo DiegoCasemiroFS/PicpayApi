04c6f5bcfcf9e5eab44630c5843d175d47112e45
DiegoCasemiroFS <dcfs1998@gmail.com>
2024-07-17 20:07:02 -0300

    feat: create TransactionServiceTest.java

---
+++ src/test/java/br/com/DiegoCasemiroFS/PicpayApi/service/TransactionServiceTest.java
@@ -0,0 +1,12 @@
+package br.com.DiegoCasemiroFS.PicpayApi.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionServiceTest {
+
+    @Test
+    void createTransaction() {
+    }
+}
